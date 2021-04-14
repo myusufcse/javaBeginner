2c6fff9e3f04ce270bf2a57ad68ca828a869912c
Yusuf Muhammed <myusufcse@users.noreply.github.com>
2021-04-14 21:50:24 +0200

    Added programs for Date and Time.

---
+++ src/DateAndTime.java
@@ -0,0 +1,44 @@
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+public class DateAndTime {
+    public static void main(String[] args) {
+        LocalDate lDate = LocalDate.now();
+        LocalDate lDate1 = LocalDate.of(2015,8,05 );
+        System.out.println(lDate);
+        System.out.println(lDate1.getDayOfWeek());
+        System.out.println(lDate.isAfter(lDate1));
+
+        LocalTime lTime = LocalTime.now();
+        System.out.println(lTime);
+        System.out.println(lTime.getHour());
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        LocalDateTime lDateTime = LocalDateTime.now();
+        System.out.println(lDateTime);
+        System.out.println(lDateTime.minusNanos(1000000));
+
+        lDateTime = LocalDateTime.of(lDate,lTime);
+        System.out.println(lDateTime);
+
+        LocalDate birthDate = LocalDate.of(1990, Month.SEPTEMBER,24);
+        Period age = Period.between(birthDate,lDate);
+
+        System.out.println("My age is : "+age);
+
+
+        System.out.println(Duration.ofHours(1).getSeconds());
+        System.out.println(Instant.now());
+
+        ZoneId be = ZoneId.systemDefault();
+        ZoneId in = ZoneId.of("UTC+05:30");
+        ZonedDateTime inTime = ZonedDateTime.of(lDateTime,be);
+        System.out.println("BE Time : "+inTime);
+        System.out.println("IN Time : "+inTime.withZoneSameInstant(in));
+        System.out.println("IN Time : "+inTime.withZoneSameInstant(in).getHour());
+
+    }
+}
---
+++ src/Solution.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sin = new Scanner(System.in);
+        int n = sin.nextInt();
+        sin.nextLine();
+
+        System.out.println(n);
+        sin.close();
+    }
+}
